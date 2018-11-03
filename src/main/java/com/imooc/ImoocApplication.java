ff0882ac45100188b08536f322152b5518ecb11d
yangchangxing <1227724725@qq.com>
2018-11-03 17:49:26 +0800

    测试

--- src/main/java/com/imooc/ImoocApplication.java
+++ src/main/java/com/imooc/ImoocApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ImoocApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ImoocApplication.class, args);
 	}
